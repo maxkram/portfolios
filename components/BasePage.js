27f1c3b8e26dca5d7610e67ab703721db59f7870
maxkram <max.kramer@live.com>
2019-05-19 11:59:37 -0400

    server authentication

---
+++ components/BasePage.js
@@ -0,0 +1,17 @@
+import { Container } from "reactstrap";
+import PropTypes from "prop-types";
+
+const BasePage = props => {
+  const { className } = props;
+  // const className = props.className || "";
+  return (
+    <div className={`base-page ${className}`}>
+      <Container>{props.children}</Container>
+    </div>
+  );
+};
+BasePage.defaultProps = {
+  className: ""
+};
+// BasePage.propTypes = { className: PropTypes.any.isRequired };
+export default BasePage;
---
+++ server/services/auth.js
@@ -0,0 +1,33 @@
+const jwt = require("express-jwt");
+const jwksRsa = require("jwks-rsa");
+
+const config = require("../config");
+const NAMESPACE = config.NAMESPACE;
+
+// MIDDLEWARE
+exports.checkJWT = jwt({
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 50,
+    jwksUri: "https://maxkram.auth0.com/.well-known/jwks.json"
+  }),
+  audience: "3ShmnWBfPk9MI8tV6SQmnOdkY7tlgoKF",
+  issuer: "https://maxkram.auth0.com/",
+  algorithms: ["RS256"]
+});
+
+exports.checkRole = role => (req, res, next) => {
+  const user = req.user;
+
+  if (user && user[NAMESPACE + "/role"] && user[NAMESPACE + "/role"] === role) {
+    next();
+  } else {
+    return res
+      .status(401)
+      .send({
+        title: "Not Authorized",
+        detail: "You are not authorized to access this data"
+      });
+  }
+};
