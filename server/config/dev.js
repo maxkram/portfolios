ea8838232ce0e6b4564dea38827f61b1b3481592
maxkram <max.kramer@live.com>
2019-05-20 10:41:06 -0400

    fixes env vars of namespace on server and axios base url on client

--- server/config/dev.js
+++ server/config/dev.js
@@ -1,3 +1,4 @@
 module.exports = {
-  DB_URI: "mongodb://maxkram:Test2019!@ds157956.mlab.com:57956/portfolio"
+  DB_URI: "mongodb://maxkram:Test2019!@ds157956.mlab.com:57956/portfolio",
+  NAMESPACE: "https://maxkram.herokuapp.com"
 };
